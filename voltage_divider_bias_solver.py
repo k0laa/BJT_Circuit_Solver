0f8837c0e2989bfba23e68f44f0563f39913e7c7
bugra <bugrayalcn1@gmail.com>
2024-12-22 20:46:52 +0300

    feat: added Ib current finder method

---
+++ voltage_divider_bias_solver.py
@@ -0,0 +1,26 @@
+# voltages
+Ve, Vb, Vc, Vce, Vbe = 0.0, 0.0, 0.0, 0.0, 0.7
+
+# currents
+Ie, Ib, Ic = 0.0, 0.0, 0.0
+
+# resistors
+Re, Rb1, Rb2, Rc = 0.0, 0.0, 0.0, 0.0
+
+# betas
+beta = 0.0
+
+
+def find_Ib(Vb, Vbe, Rb1, Rb2, Re, beta):
+    Vth = find_Vth(Vb, Rb1, Rb2)
+    Rth = find_Rth(Rb1, Rb2)
+    return (Vth - Vbe) / (Rth + (beta + 1) * Re)
+
+
+def find_Vth(Vb, Rb1, Rb2):
+    return Vb / (Rb1 + Rb2) * Rb2
+
+
+def find_Rth(Rb1, Rb2):
+    return Rb1 * Rb2 / (Rb1 + Rb2)
+

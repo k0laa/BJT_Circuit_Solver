92d5881b08b25a662e0dfbb029f4ec4efbc42606
bugra <bugrayalcn1@gmail.com>
2024-12-22 21:11:17 +0300

    feat: added Vb,Vc,Ve,Vce finder methods

--- voltage_divider_bias_solver.py
+++ voltage_divider_bias_solver.py
@@ -1,5 +1,5 @@
 # voltages
-Ve, Vb, Vc, Vce, Vbe = 0.0, 0.0, 0.0, 0.0, 0.7
+Vcc, Ve, Vb, Vc, Vce, Vbe = 0.0, 0.0, 0.0, 0.0, 0.0, 0.7
 
 # currents
 Ie, Ib, Ic = 0.0, 0.0, 0.0
@@ -32,5 +32,26 @@ def find_Ic(Ib, beta):
 def find_Ie(Ib, Ic):
     return Ib + Ic
 
+
 def find_Ie_onBeta(Ib, beta):
     return Ib * (beta + 1)
+
+
+def find_Vce(Vc, Ve):
+    return Vc - Ve
+
+
+def find_Vc(Vcc, Ic, Rc):
+    return Vcc - (Ic * Rc)
+
+
+def find_Ve(Ie, Re):
+    return Ie * Re
+
+
+def find_Vb(Vth, Ib, Rth):
+    return Vth - (Ib * Rth)
+
+
+def find_Vb_onDiode(Ve):
+    return Ve + 0.7
